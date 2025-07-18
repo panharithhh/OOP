a616b7b5c7b0aa014a980227160ac35620e53bc5
panharithhh <161872594+panharithhh@users.noreply.github.com>
2025-07-18 20:09:11 +0700

    backup before oopsie

--- backend/test.py
+++ backend/test.py
@@ -33,16 +33,18 @@ def get_db():
 async def index_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-
+@app.get("/authentication")
+async def authentication(request : Request):
+    pass
 @app.get("/admin")
 async def admin(request: Request):
     return templates.TemplateResponse("admin.html", {"request": request})
 
 @app.post("/test")
 async def login_user(
-    email: Annotated[str, Form()], 
+    email: Annotated[str, Form()],
     password: Annotated[str, Form()],
-    db: mysql.connector.MySQLConnection = Depends(get_db)
+    db: mysql.connector.MySQLConnection = Depends(get_db),
 ):
     cursor = db.cursor()
     query = "SELECT * FROM newestone.users WHERE email = %s"
@@ -53,38 +55,32 @@ async def login_user(
     if not data or data[3] != password:
         return RedirectResponse(url="/admin", status_code=status.HTTP_303_SEE_OTHER)
 
-    return RedirectResponse(url="/restaurants", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
-@app.get("/restaurants")
-async def show_restaurants_page(request: Request, db: mysql.connector.MySQLConnection = Depends(get_db)):
-    cursor = db.cursor()
+@app.get("/dashboard")
+async def dashboard(
+    request: Request,
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
+    cursor = db.cursor(dictionary=True)
     
-    cursor.execute("SELECT * FROM newestone.restaurants")
+    q = "select * from newestone.restaurants"
+    cursor.execute(q)
+    restaurant_data = cursor.fetchall()
     
-    columns = []
-    for column in cursor.description:
-        columns.append(column[0])
+    q2 = "select * from newestone.menu_items"
+    cursor.execute(q2)
+    menu_data = cursor.fetchall()
     
-    restaurant_data = []
-    for row in cursor.fetchall():
-        restaurant_data.append(dict(zip(columns, row)))
-
-    cursor.execute("SELECT id, name, event_name, event_description, event_datetime FROM newestone.restaurants")
-    
-    columns_ev = []
-    for col in cursor.description:
-        columns_ev.append(col[0])
-
-    event_data = []
-    for row in cursor.fetchall():
-        event_data.append(dict(zip(columns_ev, row)))
-
-    cursor.close()
+    cursor.execute("select * from newestone.bookings") 
+    bookings = cursor.fetchall()
+    cursor.close() 
     return templates.TemplateResponse("db.html", {
         "request": request,
         "restaurants": restaurant_data,
-        "events": event_data,
-        "active_tab": "restaurants"
+        "menu_data": menu_data,
+        "bookings" : bookings, 
+        "active_tab" : "tab" 
     })
 
 @app.post("/send")
@@ -93,7 +89,7 @@ async def send_data_of_restaurant(
     description: Annotated[str, Form()],
     address: Annotated[str, Form()],
     pictures: Annotated[List[UploadFile], File()],
-    db: mysql.connector.MySQLConnection = Depends(get_db)
+    db: mysql.connector.MySQLConnection = Depends(get_db),
 ):
     upload_folder = "../frontend/uploads"
     cursor = db.cursor()
@@ -118,7 +114,7 @@ async def send_data_of_restaurant(
 
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/restaurants", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/editrestaurant")
 async def edit_restaurant(
@@ -127,7 +123,7 @@ async def edit_restaurant(
     description: Annotated[str, Form()],
     address: Annotated[str, Form()],
     pictures: Annotated[List[UploadFile], File()],
-    db: mysql.connector.MySQLConnection = Depends(get_db)
+    db: mysql.connector.MySQLConnection = Depends(get_db),
 ):
     upload_folder = "../frontend/uploads"
     cursor = db.cursor()
@@ -147,47 +143,18 @@ async def edit_restaurant(
 
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/restaurants", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/delete-restaurant")
-async def delete_by_id(id: Annotated[int, Form()], db: mysql.connector.MySQLConnection = Depends(get_db)):
+async def delete_by_id(
+    id: Annotated[int, Form()],
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
     cursor = db.cursor()
     cursor.execute("DELETE FROM newestone.restaurants WHERE id = %s", (id,))
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/restaurants", status_code=status.HTTP_303_SEE_OTHER)
-
-@app.get("/events")
-async def show_events_page(request: Request, db: mysql.connector.MySQLConnection = Depends(get_db)):
-    cursor = db.cursor()
-    
-    cursor.execute("SELECT id, name, event_name, event_description, event_datetime FROM newestone.restaurants")
-    
-    columns = []
-    for column in cursor.description:
-        columns.append(column[0])
-        
-    event_data = []
-    for row in cursor.fetchall():
-        event_data.append(dict(zip(columns, row)))
-
-    cursor.execute("SELECT id, name FROM newestone.restaurants")
-    
-    columns_rest = []
-    for col in cursor.description:
-        columns_rest.append(col[0])
-
-    restaurants = []
-    for row in cursor.fetchall():
-        restaurants.append(dict(zip(columns_rest, row)))
-
-    cursor.close()
-    return templates.TemplateResponse("db.html", {
-        "request": request,
-        "events": event_data,
-        "restaurants": restaurants,
-        "active_tab": "special-events"
-    })
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/add-event")
 async def add_event(
@@ -195,85 +162,117 @@ async def add_event(
     name: Annotated[str, Form()],
     event_description: Annotated[str, Form()],
     datetime: Annotated[str, Form()],
-    db: mysql.connector.MySQLConnection = Depends(get_db)
+    db: mysql.connector.MySQLConnection = Depends(get_db),
 ):
     cursor = db.cursor()
     update_query = "UPDATE newestone.restaurants SET event_name = %s, event_description = %s, event_datetime = %s WHERE id = %s"
     cursor.execute(update_query, (name, event_description, datetime, restaurant_id))
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/events", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/delete-event")
-async def delete_event_by_id(id: Annotated[int, Form()], db: mysql.connector.MySQLConnection = Depends(get_db)):
+async def delete_event_by_id(
+    id: Annotated[int, Form()],
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
     cursor = db.cursor()
     cursor.execute("DELETE FROM newestone.restaurants WHERE id = %s", (id,))
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/events", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/clear-event")
-async def clear_event(id: Annotated[int, Form()], db: mysql.connector.MySQLConnection = Depends(get_db)):
+async def clear_event(
+    id: Annotated[int, Form()],
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
     cursor = db.cursor()
     clear_query = "UPDATE newestone.restaurants SET event_name = NULL, event_description = NULL, event_datetime = NULL WHERE id = %s"
     cursor.execute(clear_query, (id,))
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/events", status_code=status.HTTP_303_SEE_OTHER)
-
-@app.get("/menu")
-async def return_menu(request: Request, db: mysql.connector.MySQLConnection = Depends(get_db)):
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM newestone.menu_items")
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
     
-    columns = []
-    for column in cursor.description:
-        columns.append(column[0])
-        
-    menu_data = []
-    for row in cursor.fetchall():
-        menu_data.append(dict(zip(columns, row)))
-        
-    cursor.close()
 
-    return templates.TemplateResponse("db.html", {
-        "request": request,
-        "menu_data": menu_data,
-        "active_tab": "menu"
-    })
-
-@app.post("/add-menu-item")
-async def add_menu(db: mysql.connector.MySQLConnection = Depends(get_db)):
+@app.post("/delete-menu-item")
+async def delete_item(id : Annotated[int, Form()], db: mysql.connector.MySQLConnection = Depends(get_db)):
     cursor = db.cursor()
+    cursor.execute("delete from newestone.menu_items where id = %s",(id,))
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/menu", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
-@app.get("/delete")
-async def delete_item(db: mysql.connector.MySQLConnection = Depends(get_db)):
+@app.post("/confirm-booking")
+async def confirm_booking(
+    id : Annotated[int, Form()],     
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
     cursor = db.cursor()
+    cursor.execute ("Update newestone.bookings set status = 'confirmed' where id = %s",(id,) )
     db.commit()
     cursor.close()
-    return RedirectResponse(url="/menu", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
-@app.post("/confirmation")
-async def confirm_booking():
-    pass
+@app.post("/pend-booking")
+async def cancel_booking(
+    id : Annotated[int, Form()],     
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
+    cursor = db.cursor()
+    cursor.execute("Update newestone.bookings set status = 'pending' where id = %s",(id,) )
+    db.commit()
+    cursor.close()
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
-@app.post("/cancel")
-async def cancel_booking():
-    pass
+@app.post("/cancel-booking")
+async def cancel_booking(
+    id : Annotated[int, Form()],     
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
+    cursor = db.cursor()
+    cursor.execute("Update newestone.bookings set status = 'cancelled' where id = %s",(id,) )
+    db.commit()
+    cursor.close()
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/clear-the-booking-board")
 async def clear_booking_board():
     pass
 
-@app.post("/authentication")
-async def authentication():
-    pass
 
+@app.post("/add-menu-item")
+async def add_menu_item(
+    restaurant_id: Annotated[int, Form()],
+    name: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    price: Annotated[float, Form()],
+    picture: Annotated[UploadFile, File()],
+    db: mysql.connector.MySQLConnection = Depends(get_db),
+):
+    upload_folder = "../frontend/uploads"
+    os.makedirs(upload_folder, exist_ok=True)
 
-@app.post("/dashboard")
-async def dashboard(request = Request):
+    file_location = os.path.join(upload_folder, picture.filename)
     
-    pass
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(picture.file, buffer)
+        
+    image_url_path = f"/uploads/{picture.filename}"
+
+    cursor = db.cursor()
+    
+    query = """
+        INSERT INTO newestone.menu_items 
+        (item_name, description, price, image_url, restaurant_id) 
+        VALUES (%s, %s, %s, %s, %s)
+    """
+    
+    cursor.execute(query, (name, description, price, image_url_path, restaurant_id))
+    
+    db.commit()
+    
+    cursor.close()
+    
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+
